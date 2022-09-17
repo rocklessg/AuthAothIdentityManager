9bc0876a42d6b5516c97cacdcf020136a9867746
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-09-17 18:13:40 +0100

    Add project files.

---
+++ AuthAothIdentityManager/Program.cs
@@ -0,0 +1,38 @@
+using AuthAothIdentityManager.Models.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(op =>
+op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// AddEntityFrameworkStores adds relationship between user, roles and stores for the db context
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+// WARNING: Authentication should always comes before Authorizations in the HTTP request pipeline
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
